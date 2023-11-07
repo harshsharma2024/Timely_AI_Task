eb46553c361f463ad2622a6fbdb798758672616d
harshsharma2024 <harshsharma2024@gmail.com>
2023-11-07 16:18:26 +0530

    database added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const WebSocket = require('ws');
 const { queue } = require('./bot.js');
 // Configure a DataBase
+const sqlite3 = require('sqlite3').verbose();
+
 
 
 const PORT = 5000;
@@ -8,6 +10,9 @@ const PORT = 5000;
 const wsServer = new WebSocket.Server({
     port: PORT
 });
+
+const db = new sqlite3.Database('messages.db');
+
 
 wsServer.on('connection', function (socket) {
     console.log("A client just connected");
@@ -26,6 +31,17 @@ wsServer.on('connection', function (socket) {
     //     await queue.add({ firstName: 'Server', text: msg });
     // });
 
+    //  Getting messages from Database
+    db.all("SELECT * FROM messages", (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        rows.forEach(row => {
+            socket.send(row.text);
+        });
+    });
+
     socket.on('close', function () {
         console.log('Client disconnected');
     })
@@ -36,6 +52,14 @@ queue.process(async (job) => {
     try {
         const messageContent = job.data;
         console.log(`Message received from ${messageContent.firstName}: ${messageContent.text}`);
+
+        // Store the message in the SQLite database
+        db.run("INSERT INTO messages (firstName, text) VALUES (?, ?)", [messageContent.firstName, messageContent.text], function(err) {
+            if (err) {
+                return console.error(err.message);
+            }
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
 
         // Send the message to all connected clients
         wsServer.clients.forEach(function (client) {
